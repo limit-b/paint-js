0d40790b57667c198ea6a6fe8c35dbab22110a1f
limit-a <wjswogks152@gmail.com>
2021-03-10 23:25:56 +0900

    Add_2D_context-210310

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
 const jsCanvas = document.getElementById('js-canvas');
+const ctx = jsCanvas.getContext('2d');
+
+jsCanvas.width = 700;
+jsCanvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = '2.5';
 
 let painting = false;
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(event) {
   painting = true;
-  console.log(event, painting);
+}
+
+function startPainting() {
+  painting = true;
 }
 
 function stopPainting() {
   painting = false;
 }
 
-function onMouseUp(event) {
-  stopPainting();
-  console.log(event, painting);
-}
-
 function init() {
   if (jsCanvas) {
     jsCanvas.addEventListener('mousemove', onMouseMove);
-    jsCanvas.addEventListener('mousedown', onMouseDown);
-    jsCanvas.addEventListener('mouseup', onMouseUp);
+    jsCanvas.addEventListener('mousedown', startPainting);
+    jsCanvas.addEventListener('mouseup', stopPainting);
     jsCanvas.addEventListener('mouseleave', stopPainting);
   }
 }
